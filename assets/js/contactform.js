358259e8f4414e682665eb18dd5102fa99ba8f21
Denis <41701228+danmiles@users.noreply.github.com>
2021-10-01 19:40:52 +0200

    01/10/21 19:40

--- assets/js/contactform.js
+++ assets/js/contactform.js
@@ -1,17 +1,17 @@
 // PHP PHP PHP PHP PHP PHP PHP PHP PHP PHP PHP PHP PHP PHP PHP PHP PHP PHP PHP PHP
 
 function sendRequest(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  if (validateemail(),checkdeuxemails()) {
-      fetch("contact.php", {
-              method: "POST",
-              body: new FormData(document.querySelector('form'))
-          })
+    if (validateemail(), checkdeuxemails()) {
+        fetch("contact.php", {
+            method: "POST",
+            body: new FormData(document.querySelector('form'))
+        })
 
-          .then((data) => data.json())
-          .then((json) => console.log(json.response))
-  }
+            .then((data) => data.json())
+            .then((json) => console.log(json.response))
+    }
 }
 
 // VALIDATE VALIDATE VALIDATE VALIDATE VALIDATE VALIDATE VALIDATE VALIDATE VALIDATE VALIDATE
@@ -21,16 +21,16 @@ inputemail.addEventListener("change", validateemail);
 
 function validateemail() {
 
-  var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-  $("#email1").removeClass("is-valid");
-  $("#email1").removeClass("is-invalid");
+    $("#email1").removeClass("is-valid");
+    $("#email1").removeClass("is-invalid");
 
-  if (inputemail.value.match(mailformat)) {
-    $("#email1").addClass("is-valid");
-  } else {
-    $("#email1").addClass("is-invalid");
-  }
+    if (inputemail.value.match(mailformat)) {
+        $("#email1").addClass("is-valid");
+    } else {
+        $("#email1").addClass("is-invalid");
+    }
 }
 
 // COMPARE COMPARE COMPARE COMPARE COMPARE COMPARE COMPARE COMPARE COMPARE COMPARE
@@ -40,12 +40,12 @@ inputemaildeux.addEventListener("change", checkdeuxemails);
 
 function checkdeuxemails() {
 
-  $("#email1,#email2").removeClass("is-valid");
-  $("#email1,#email2").removeClass("is-invalid");
+    $("#email1,#email2").removeClass("is-valid");
+    $("#email1,#email2").removeClass("is-invalid");
 
-  if (document.querySelector("#email1").value === document.querySelector("#email2").value) {
-    $("#email1,#email2").addClass("is-valid");
-  } else {
-      $("#email1,#email2").addClass("is-invalid");
+    if (document.querySelector("#email1").value === document.querySelector("#email2").value) {
+        $("#email1,#email2").addClass("is-valid");
+    } else {
+        $("#email1,#email2").addClass("is-invalid");
     }
 }
--- contact.php
+++ contact.php
@@ -5,15 +5,14 @@ $email = ($_POST['text1']);
 $sujet = ($_POST['sujet']);
 $message = ($_POST['message']);
 
-$to = "umfrancois@gmail.com";
+$to = "dannnn163@gmail.com";
 $body = "De la part de : $nom $prenom, $email \n\nMessage: \n$message \n\nCordialement, $nom $prenom.";
 
 if (!empty($email) && !empty($message)) {
-  if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    mail($to, $sujet, $body);
-    echo "Message envoyé";
-  } else {
-    echo "Message non envoyé";
-  }
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        mail($to, $sujet, $body);
+        header('Location: mail-send.html');
+    } else {
+        header('Location: mail-failed.html');
+    }
 }
-?>
